cfb1dd055fc900db7b695831667afd2c474934e4
Elisabeth D <elisabeth.degrand@hotmail.fr>
2018-02-26 17:25:52 +0100

    Add 3.3 and 3.4 parts, and small modifications

--- find_convergence.m
+++ find_convergence.m
@@ -5,40 +5,35 @@ function [ Y ] = find_convergence( X,W , mode)
 
 %preparation of the plotting
 n = sqrt(length(X));
-xrange = linspace(0,1,n);
+xrange = linspace(1,0,n);
 [xx1, xx2] = meshgrid(xrange,xrange);
 XGrid = [xx1(:) xx2(:)];
-figure()
-%hold on
 
 Y = X;
 X=zeros(size(X));
 cptr = 0; %counts the number of iterations
-while any(X(:)~=Y(:))
+energy= [-Y*W*Y']; %energy of the system at each iteration
+while any(X(:)~=Y(:)) && cptr < 10000
     X=Y;
     if mode == 0
         Y=update_rule(X,W);
+        cptr= cptr + 1;
+        energy= [energy;-Y*W*Y'];
     else
-        for i = 1:100
+        for i = 1:1000
             Y = update_sequential(Y,W);
+            if mod(cptr,100)==0
+                g=gscatter(XGrid(:,1), XGrid(:,2),X,[0.9 0.9 0.9;0.1 0.1 0.1]);
+                g(1).MarkerSize=30;
+                g(2).MarkerSize=30;
+                pause(0.001)
+            end
+            cptr= cptr + 1;
+            energy= [energy;-Y*W*Y'];
         end
-    
-    
-    disp(cptr)
-    g=gscatter(XGrid(:,1), XGrid(:,2),X,[0.9 0.9 0.9;0.1 0.1 0.1]);
-    g(1).MarkerSize=30;
-    g(2).MarkerSize=30;
-    pause(0.2)
     end
-    
-    cptr= cptr + 1;
-    
+      
 end
-if mode==1
-    g=gscatter(XGrid(:,1), XGrid(:,2), Y,[0.9 0.9 0.9;0.1 0.1 0.1]);
-    g(1).MarkerSize=30;
-    g(2).MarkerSize=30;
-    title('Final figure')
-end
+% plot(energy)
 end
 
--- main3_2.m
+++ main3_2.m
@@ -1,29 +1,61 @@
 % loading the pictures
 pict=load('./pict.dat');
 pict = reshape(pict, [1024,11])';
-
+nb_units = length(pict);
 
 % preparation for the plotting of the figures
 n = sqrt(length(pict));
 
-xrange = linspace(0,1,n);
+xrange = linspace(1,0,n);
 [xx1, xx2] = meshgrid(xrange,xrange);
 XGrid = [xx1(:) xx2(:)];
+
+%figure that we want
+nb_fig = 1;
+X_org = pict(nb_fig,:); %original picture
 
 % Computations
 X=[pict(1,:); pict(2,:); pict(3,:)];
 W = compute_weights(X);
-pict_after = find_convergence(pict(10,:),W,1);
+% pict_after = find_convergence(pict(nb_fig,:),W,0);
+% pict_after = update_rule(X_org,W);
+
+%random computations (for 3.3)
+% W = randn(length(pict));
+% W = 0.5*(W+W');
+% X =sign(randn(1,length(pict)));
+% pict_after = find_convergence(X,W,1);
+
+
+% Measure of distortion (for 3.4)
+rng(3);
+range = 1:nb_units;
+error = zeros(size(range));
+for noise_per = range
+    ind = randperm(length(1:nb_units), noise_per); %choose noise_per indices
+    X = X_org;
+    X(ind) = -X(ind);     %flip the number of this combination  
+%     pict_after = find_convergence(X,W,0);
+    pict_after = update_rule(X,W);
+    error(noise_per) = sum(abs(pict_after - X_org))/2;
+end
+figure(1)
+hold on
+plot(linspace(1,100,length(range)),error);
+set(gca,'FontSize',13)
+xlabel('Percentage of noise')
+ylabel('Number of wrong pixels')
 
 %Plotting
-figure()   % plotting of the original figure
-% hold on
-g=gscatter(XGrid(:,1), XGrid(:,2), pict(1,:),[0.9 0.9 0.9;0.1 0.1 0.1]);
-g(1).MarkerSize=30;
-g(2).MarkerSize=30;
+% figure()
+% g=gscatter(XGrid(:,1), XGrid(:,2), X_org,[0.9 0.9 0.9;0.1 0.1 0.1]);
+% g(1).MarkerSize=30;
+% g(2).MarkerSize=30;
 % 
+% % 
 % figure()  %plotting after the network
 % hold on
 % g=gscatter(XGrid(:,1), XGrid(:,2), pict_after,[0.9 0.9 0.9;0.1 0.1 0.1]);
 % g(1).MarkerSize=30;
 % g(2).MarkerSize=30;
+% title('final figure')
